acd2c0068daab5e042ca93963017e609ac70a88c
Martin Salomon <salomonmartin21@gmail.com>
2023-10-09 15:05:45 -0300

    Updates

---
+++ precache-manifest.72934fa40298cd1a91c188928346e062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef66b1cac0ab6610a61c548050f94f1",
+    "url": "/curso-react-intro/index.html"
+  },
+  {
+    "revision": "029e46b0f95f7fec125b",
+    "url": "/curso-react-intro/static/css/main.4080c7dd.chunk.css"
+  },
+  {
+    "revision": "a122c5d1de6df0c84187",
+    "url": "/curso-react-intro/static/js/2.982bc7fd.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/curso-react-intro/static/js/2.982bc7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029e46b0f95f7fec125b",
+    "url": "/curso-react-intro/static/js/main.abc8cf7a.chunk.js"
+  },
+  {
+    "revision": "056824fdab9d7122fce9",
+    "url": "/curso-react-intro/static/js/runtime-main.132aa7c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/curso-react-intro/precache-manifest.8f3e5f1b22eb2f9f4d155908dd19d684.js"
+  "/curso-react-intro/precache-manifest.72934fa40298cd1a91c188928346e062.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/curso-react-intro/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/curso-react-intro/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
